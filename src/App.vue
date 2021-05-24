759d9c282a615fb01fbb409f09f67c2b62f6b172
ckattmann <christoph.kattmann@bss-hs.de>
2021-05-24 08:19:29 +0200

    Added Routing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang='pug'>
+
+#app
+    Topbar
+    router-view
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Topbar from './components/Topbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Map,
+    Topbar
+  },
+  mounted() {
   }
 }
 </script>
 
 <style>
+
+@font-face {
+    font-family: 'Open Sans';
+    src: url('assets/OpenSans-Regular.ttf');
+}
+
+html, body {
+  margin: 0;
+  min-height: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
 </style>
